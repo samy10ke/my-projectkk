54383f1df8200222b4d85913a3b58794ead35252
samy10ke <152733046+samy10ke@users.noreply.github.com>
2023-12-03 13:43:23 +0330

    Add files via upload

---
+++ tamrin 4_soal B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+
+ main() {
+int x,y,z;
+float a,b,c;
+cout<<"pls inter three interger value :"<<"\n";
+cin>>x>>y>>z;
+a=(x-y)*(x+y);
+b=(x+y+z)*(x+y+z)+1;
+c=a/c;
+cout<<"your answer is : "<<c;
+}
